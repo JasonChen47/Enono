1f36c1965e9703dd812294f16c4ecf93ce955058
JasonChen47 <jwc297@cornell.edu>
2023-11-08 17:47:49 -0800

    added people page on home

--- lib/home_tab.dart
+++ lib/home_tab.dart
@@ -18,23 +18,7 @@ class HomeTab extends StatefulWidget {
 }
 
 class _HomeTabState extends State<HomeTab> {
-  static const _itemsLength = 20;
-
-  late final List<Color> colors;
-  late final List<String> titles;
-  late final List<String> contents;
-
-  @override
-  void initState() {
-    colors = getRandomColors(_itemsLength);
-    titles = List.generate(_itemsLength, (index) => generateRandomHeadline());
-    contents =
-        List.generate(_itemsLength, (index) => lorem(paragraphs: 1, words: 24));
-    super.initState();
-  }
-
   Widget _tabBar(BuildContext context) {
-    // return Center(child: Text('hi'));
     return DefaultTabController(
       initialIndex: 0,
       length: 2,
@@ -52,14 +36,12 @@ class _HomeTabState extends State<HomeTab> {
             ],
           ),
         )),
-        body: const TabBarView(
+        body: TabBarView(
           children: <Widget>[
             Center(
-              child: Text("Add projects here"),
+              child: ProjectsPage(),
             ),
-            Center(
-              child: Text("Add people here"),
-            )
+            Center(child: PeoplePage())
           ],
         ),
       ),
@@ -76,9 +58,7 @@ class _HomeTabState extends State<HomeTab> {
 
   Widget _buildIos(BuildContext context) {
     return CupertinoPageScaffold(
-        navigationBar: const CupertinoNavigationBar(),
-        // child: Text('hi')
-        child: _tabBar(context));
+        navigationBar: const CupertinoNavigationBar(), child: _tabBar(context));
   }
 
   @override
@@ -93,13 +73,95 @@ class _HomeTabState extends State<HomeTab> {
 class ProjectsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('Projects');
+    return const Text('Projects');
   }
 }
 
-class PeoplePage extends StatelessWidget {
+class PeoplePage extends StatefulWidget {
+  static const _itemsLength = 20;
+
+  @override
+  State<PeoplePage> createState() => _PeoplePageState();
+}
+
+class _PeoplePageState extends State<PeoplePage>
+    with AutomaticKeepAliveClientMixin {
+  late final List<Color> colors;
+  late final List<String> titles;
+  late final List<String> contents;
+
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  void initState() {
+    colors = getRandomColors(PeoplePage._itemsLength);
+    titles = List.generate(
+        PeoplePage._itemsLength, (index) => generateRandomHeadline());
+    contents = List.generate(
+        PeoplePage._itemsLength, (index) => lorem(paragraphs: 1, words: 24));
+    super.initState();
+  }
+
+  Widget _listBuilder(BuildContext context, int index) {
+    return SafeArea(
+      top: false,
+      bottom: false,
+      child: Card(
+        elevation: 1.5,
+        margin: const EdgeInsets.fromLTRB(6, 12, 6, 0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4),
+        ),
+        child: InkWell(
+          // Make it splash on Android. It would happen automatically if this
+          // was a real card but this is just a demo. Skip the splash on iOS.
+          onTap: defaultTargetPlatform == TargetPlatform.iOS ? null : () {},
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  backgroundColor: colors[index],
+                  child: Text(
+                    titles[index].substring(0, 1),
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+                const Padding(padding: EdgeInsets.only(left: 16)),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        titles[index],
+                        style: const TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      const Padding(padding: EdgeInsets.only(top: 8)),
+                      Text(
+                        contents[index],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('People');
+    super.build(context);
+    return ListView.builder(
+      itemCount: PeoplePage._itemsLength,
+      itemBuilder: _listBuilder,
+    );
   }
 }
