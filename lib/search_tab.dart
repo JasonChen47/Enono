d10fc22b20813a0ef44b283d5e3d7a37a2de29c9
JasonChen47 <jwc297@cornell.edu>
2023-11-09 20:52:41 -0800

    added search bar

--- lib/search_tab.dart
+++ lib/search_tab.dart
@@ -6,18 +6,59 @@ class SearchTab extends StatefulWidget {
   static const title = 'Search';
   static const androidIcon = Icon(Icons.search);
   static const iosIcon = Icon(CupertinoIcons.search);
+  const SearchTab({super.key});
 
   @override
   State<SearchTab> createState() => _SearchTabState();
 }
 
 class _SearchTabState extends State<SearchTab> {
+  TextEditingController textController = TextEditingController();
+
   @override
-  Widget build(context) {
-    return Center(
-      child: Container(
-        child: Text('Search Page'),
-      ),
-    );
+  void initState() {
+    super.initState();
+    textController = TextEditingController(text: 'initial text');
   }
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        navigationBar: const CupertinoNavigationBar(
+          middle: Text('Search'),
+        ),
+        // child: Container(
+        //   child: Padding(
+        //     padding: const EdgeInsets.all(16.0),
+        //     child: CupertinoSearchTextField(
+        //       controller: textController,
+        //       placeholder: 'Search',
+        //     ),
+        //   ),
+        // ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CupertinoSearchTextField(
+                //       controller: textController,
+                //       placeholder: 'Search',
+                ),
+          ),
+        ));
+  }
+
+  // @override
+  // Widget build(context) {
+  //   return Center(
+  //     child: Container(
+  //       child: Text('Search Page'),
+  //     ),
+  //   );
+  // }
 }
