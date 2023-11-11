23041b3f605646ef8c4b54250b3287a8de765841
JasonChen47 <jwc297@cornell.edu>
2023-11-10 16:22:07 -0800

    built basic UI for chat page

--- lib/chat_tab.dart
+++ lib/chat_tab.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'widgets.dart';
+
+class ChatUsers {
+  String name;
+  String messageText;
+  String imageURL;
+  String time;
+  ChatUsers(
+      {required this.name,
+      required this.messageText,
+      required this.imageURL,
+      required this.time});
+}
 
 class ChatTab extends StatefulWidget {
   static const title = 'Chat';
@@ -12,17 +25,103 @@ class ChatTab extends StatefulWidget {
 }
 
 class _ChatTabState extends State<ChatTab> {
+  List<ChatUsers> chatUsers = [
+    ChatUsers(
+        name: "Jane Russel",
+        messageText: "Awesome Setup",
+        imageURL: "images/userImage1.jpeg",
+        time: "Now"),
+    ChatUsers(
+        name: "Glady's Murphy",
+        messageText: "That's Great",
+        imageURL: "images/userImage2.jpeg",
+        time: "Yesterday"),
+    ChatUsers(
+        name: "Jorge Henry",
+        messageText: "Hey where are you?",
+        imageURL: "images/userImage3.jpeg",
+        time: "31 Mar"),
+    ChatUsers(
+        name: "Philip Fox",
+        messageText: "Busy! Call me in 20 mins",
+        imageURL: "images/userImage4.jpeg",
+        time: "28 Mar"),
+    ChatUsers(
+        name: "Debra Hawkins",
+        messageText: "Thankyou, It's awesome",
+        imageURL: "images/userImage5.jpeg",
+        time: "23 Mar"),
+    ChatUsers(
+        name: "Jacob Pena",
+        messageText: "will update you in evening",
+        imageURL: "images/userImage6.jpeg",
+        time: "17 Mar"),
+    ChatUsers(
+        name: "Andrey Jones",
+        messageText: "Can you please share the file?",
+        imageURL: "images/userImage7.jpeg",
+        time: "24 Feb"),
+    ChatUsers(
+        name: "John Wick",
+        messageText: "How are you?",
+        imageURL: "images/userImage8.jpeg",
+        time: "18 Feb"),
+  ];
+
+  TextEditingController textController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    textController = TextEditingController(text: 'initial text');
+  }
+
   @override
   Widget build(context) {
     return CupertinoPageScaffold(
-        navigationBar: const CupertinoNavigationBar(
-          middle: Text('Chat'),
+      resizeToAvoidBottomInset: false,
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text('Chat'),
+      ),
+      // child: SafeArea(
+      //   child: Padding(
+      //     padding: const EdgeInsets.all(12.0),
+      //     child: CupertinoSearchTextField(
+      //       controller: textController,
+      //       placeholder: 'Search',
+      //     ),
+      //   ),
+      // ),
+
+      child: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: CupertinoSearchTextField(
+                controller: textController,
+                placeholder: 'Search',
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: chatUsers.length,
+                shrinkWrap: true,
+                padding: EdgeInsets.only(top: 16),
+                itemBuilder: (context, index) {
+                  return ConversationList(
+                    name: chatUsers[index].name,
+                    messageText: chatUsers[index].messageText,
+                    imageUrl: chatUsers[index].imageURL,
+                    time: chatUsers[index].time,
+                    isMessageRead: (index == 0 || index == 3) ? true : false,
+                  );
+                },
+              ),
+            ),
+          ],
         ),
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Text('hi'),
-          ),
-        ));
+      ),
+    );
   }
 }
